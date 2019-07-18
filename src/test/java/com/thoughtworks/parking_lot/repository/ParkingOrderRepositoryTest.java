4d182b548b7d838a7ccedaa9a988636e7f19553d
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-18 20:25:35 +0800

    create parkingOrderRepositoryTest

--- src/test/java/com/thoughtworks/parking_lot/repository/ParkingOrderRepositoryTest.java
+++ src/test/java/com/thoughtworks/parking_lot/repository/ParkingOrderRepositoryTest.java
@@ -1,5 +1,17 @@
+package com.thoughtworks.parking_lot.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
 class ParkingOrderRepositoryTest {
+
+    @Autowired
+    private ParkingOrderRepository parkingOrderRepository;
 
 }
