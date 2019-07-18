e75d2008161704b447e2af4e2fd5f07790c3eea3
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-18 16:36:50 +0800

    implement story 1 AC 1

---
+++ src/main/java/com/thoughtworks/parking_lot/entity/ParkingLot.java
@@ -0,0 +1,54 @@
+package com.thoughtworks.parking_lot.entity;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Negative;
+import javax.validation.constraints.NegativeOrZero;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "parking_lot")
+public class ParkingLot {
+
+    @UniqueElements
+    private String name;
+    private int capacity;
+    private String address;
+
+    public ParkingLot() {}
+
+    public ParkingLot(String name, int capacity, String address) {
+        this.name = name;
+        this.capacity = capacity;
+        this.address = address;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
